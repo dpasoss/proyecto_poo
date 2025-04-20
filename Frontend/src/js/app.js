e6f7686a17599fb01a4f4544cafb399692e0d555
Sofia <144188742+sofia-zuga@users.noreply.github.com>
2025-04-19 21:58:49 -0600

    Create app.js

---
+++ Frontend/src/js/app.js
@@ -0,0 +1,26 @@
+import controller from './controllers/panelEmpleador.js';
+
+
+// Listado de libros que se obtienen desde el API
+document.getElementById("btnListar").addEventListener('click',(event)=>{
+    event.preventDefault();
+    controller.obtenerTrabajos();
+});
+
+// Guardar un libro en el servidor por medio del API
+document.getElementById("btnGuardar").addEventListener('click',(event)=>{
+    event.preventDefault();
+    controller.crearTrabajo();
+});
+
+// Actualizar los datos de un libro
+document.getElementById("btnActualizar").addEventListener('click',(event)=>{
+    event.preventDefault();
+    controller.actualizarTrabajo();
+});
+
+// Actualizar los datos de un libro
+document.getElementById("btnLimpiar").addEventListener('click',(event)=>{
+    event.preventDefault();
+    controller.limpiar();
+});

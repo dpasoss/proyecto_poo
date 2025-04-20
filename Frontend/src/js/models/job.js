e3f2b95070da5d026ef25fe67b0b731835774a64
Sofia <144188742+sofia-zuga@users.noreply.github.com>
2025-04-19 21:58:19 -0600

    Create job.js

---
+++ Frontend/src/js/models/job.js
@@ -0,0 +1,12 @@
+export class Job {
+    constructor(titulo, descripcion, salario, ubicacion, tipoTrabajo, modalidad, vencimiento, id) {
+      this.titulo = titulo;
+      this.descripcion = descripcion;
+      this.salario = salario;
+      this.ubicacion = ubicacion;
+      this.tipoTrabajo = tipoTrabajo;
+      this.modalidad = modalidad;
+      this.vencimiento = vencimiento;
+      this.id = id;
+    }
+}

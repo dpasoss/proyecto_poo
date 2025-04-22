e8e6735ad4ba87eee46e18a83384f01e1722d49e
Sofia <144188742+sofia-zuga@users.noreply.github.com>
2025-04-21 19:47:43 -0600

    Update job.controller.js
    actualizar trabajo estaba doble

--- Backend/controllers/job.controller.js
+++ Backend/controllers/job.controller.js
@@ -37,17 +37,6 @@ export const obtenerTrabajoPorId = async (req, res) => {
 };
 
 
-// Actualizar un trabajo
-export const actualizarTrabajo = async (req, res) => {
-  try {
-    const trabajoActualizado = await Job.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!trabajoActualizado) return res.status(404).json({ mensaje: "Trabajo no encontrado" });
-    res.status(200).json(trabajoActualizado);
-  } catch (error) {
-    res.status(400).json({ mensaje: "Error al actualizar el trabajo", error });
-  }
-};
-
 // Eliminar un trabajo
 export const eliminarTrabajo = async (req, res) => {
   try {
